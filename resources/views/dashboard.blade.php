81966587393184ed1ec330756dd4e600fdb3e450
Paulo <pvpmartins@hotmail.com>
2023-08-31 16:04:50 -0300

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Bem vindo ao Vennx News") }}
+                    {{ __("Bem-vindo ao Vennx News") }}
                 </div>
             </div>
         </div>
